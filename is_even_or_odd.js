22a9cb405b06ea5a00934f27af3d3c6afe2e0765
Vishal Reddy <vlukereddy@gmail.com>
2015-03-01 09:56:36 -0800

    added is even or odd script

---
+++ is_even_or_odd.js
@@ -0,0 +1,21 @@
+function isOdd(number){
+  
+  if(number % 2 !== 0) {
+    console.log("The number is odd.");
+  } else {
+    console.log("The number is even.");
+  }  
+}
+
+
+function isEven(number){
+   if(number % 2 === 0){
+     console.log("The number is even.");
+   } else {
+     console.log("The number is odd");
+   }
+}
+
+
+isOdd(3);
+isEven(3);
