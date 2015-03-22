691bbf65f2667d28e9e0eae46756e579b678f9db
Vishal Reddy <vishal@getopenbook.com>
2015-03-22 10:11:15 -0700

    Outer Inner loop with array push.

---
+++ outer_inner_loop_array_push.js
@@ -0,0 +1,26 @@
+var text = "Hello everybody, I'm Peter, I love reading novels, soccer and playing piano. Peter has been blessed by the Lord and he is a living testimony that God is the God of miracles. Petras is the word for Peter in Greek. It means rock. ";
+var myName = "Peter";
+var hits = [];
+for(var i = 0; i < text.length; i++){
+  
+    console.log("----------------OUTER LOOP OPENED--------------");
+    console.log("The current value of i is: " + i);
+    console.log("The character value of text[i] is: " + text[i]);
+    if (text[i] === myName[0] ){
+        // DO something here Sammy
+        console.log("The i value is: " + i + " when the character found is P.");
+        console.log("The value inside text at index i is: " + text[i]);
+        for (var j = i;
+        j < i + myName.length; j++){
+            console.log("----------------INNER LOOP OPENED--------------");
+            console.log("The j value is: " + j);
+            console.log("The value inside text at index j: " + text[j]);
+            hits.push(text[j]);
+            console.log("----------------INNER LOOP CLOSED--------------");
+        }
+
+    }
+     console.log("----------------OUTER LOOP CLOSED--------------");   
+}
+
+console.log("The elements inside the hits array are: " + hits);
